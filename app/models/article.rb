ffe32183a62b14ff54da3a227610bb06fdd8c539
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-01 11:33:52 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,6 @@
 
 class Article < ActiveRecord::Base
 
-<<<<<<< HEAD
-=======
   attr_accessor :title, :content
 
   ARTICLES = []
@@ -18,5 +16,4 @@ class Article < ActiveRecord::Base
   def self.all
     ARTICLES
   end
->>>>>>> c6e75a756e1ed92b0d77f634332710402c6a1db9
 end
