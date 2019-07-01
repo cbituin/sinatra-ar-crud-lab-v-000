0520ada3d89a420dc379f738a9e31c5be0de7692
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-01 12:10:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/articles/:id" do
-  @article = Article.find(params[:id])
-  erb :show
-end
+    @article = Article.find(params[:id])
+    erb :show
+  end
 
 end
