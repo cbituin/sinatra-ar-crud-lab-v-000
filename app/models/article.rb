5ec8e0932374c7b61303a3fdec5248fb26c7b8fa
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-01 11:29:23 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,21 @@
 
 class Article < ActiveRecord::Base
 
+<<<<<<< HEAD
+=======
+  attr_accessor :title, :content
+
+  ARTICLES = []
+
+  def initialize(args)
+    @title = args[:title]
+    @content = args[:content]
+
+    ARTICLES << self
+  end
+
+  def self.all
+    ARTICLES
+  end
+>>>>>>> c6e75a756e1ed92b0d77f634332710402c6a1db9
 end
